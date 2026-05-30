046361ef8948a426ac2eaf13d5a29827c5b0819e
CalebKwekuOkoh <2425400891@live.gctu.edu.gh>
2026-05-30 20:04:04 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,5 +1,23 @@
-print("...CKO")
-print("       ")
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 2
+#
+# Name: Caleb Kweku Okoh
+# Student ID: 2425400891
+# Email: 2425400891@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Caleb Kweku Okoh, this programming assignment is my own
+# and have not provided this code to any other student.
+
+
+def main():
+    # Print my initials in large letters using print statements.
+    print()
+    print("...CKO")
+    print()
+
+
 print("........CCCCCCC........KK........KK............OOOO.........")
 print(".....CCC...............KK......KK...........OO......OO......")
 print("....CC.................KK.....KK...........0O........OO.....")
@@ -10,3 +28,7 @@ print("....CC.................KK..KK..............OO........OO.....")
 print("....CC.................KK....KK............OO........OO.....")
 print("......CCC.........**...KK......KK.....**.....OO....OO.....**")
 print(".........CCCCCC...**...KK........KK...**.......OOOO.......**")
+
+print()
+
+main()
